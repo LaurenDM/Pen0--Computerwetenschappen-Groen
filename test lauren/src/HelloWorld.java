9856160f12f9d9c84e09ef019843269189771edc
Lauren De Meyer <lauren.demeyer@student.kuleuven.be>
2012-10-01 18:48:47 +0200

    nog eens nieuw

--- test lauren/src/HelloWorld.java
+++ test lauren/src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(){
 		System.out.println("Hello World");
         System.out.println("New thing");
+        System.out.println("Nog eens nieuw! Het werkt echt wel! ;) ");
 	}
 	
 }
