7ce203e41a88d963843cd03fbfb212edf649fc18
francisduvivier <francisduvivier@gmail.com>
2012-10-08 12:45:09 +0200

    Blijkbaar moet de main methode verplicht in een van de klassen staan(wat ook wel vrij logisch is) en moet de string[] argds wel degelijk als parameter erin staan, anders wordt het niet herkend als main-methode.

--- test lauren/src/HelloWorld.java
+++ test lauren/src/HelloWorld.java
@@ -1,7 +1,7 @@
 
 public class HelloWorld {
 
-	public static void main(){
+	public static void main(String[] args){
 		System.out.println("Hello World");
         System.out.println("New thing");
         System.out.println("Nog eens nieuw! Het werkt echt wel! ;) ");
--- test lauren/src/Heyhey.java
+++ test lauren/src/Heyhey.java
@@ -1,5 +1,8 @@
 
 public class Heyhey {
+	public static void main(String[] args){
+		(new Heyhey()).test();
+	}
 	public void test(){
 		System.out.println("als dit werkt ben ik blij :) ");
 	}
