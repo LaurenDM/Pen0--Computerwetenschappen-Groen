57032e45cd95a59c08421fbb497a669ba981b9d9
Francis Duvivier <francisduvivier@gmail.com>
2012-10-16 20:46:02 +0200

    De moveThread gecommit

---
+++ test lauren/src/domain/robots/MoveThread.java
@@ -0,0 +1,14 @@
+package domain.robots;
+
+public class MoveThread extends Thread {
+	Movement moveType;
+	SimRobotPilot simRobotPilot;
+	public MoveThread(Movement moveType, SimRobotPilot simRobotPilot){
+		this.moveType= moveType;
+		this.simRobotPilot=simRobotPilot;
+	}
+	
+	public void run(){
+		moveType.execute(simRobotPilot);
+	}
+}
