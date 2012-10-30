86617bc547efb1025dd3f294cb844719e863ecb6
Lauren De Meyer <lauren.demeyer@student.kuleuven.be>
2012-10-30 14:49:10 +0100

    fout verbeterd in mazeelement

--- test lauren/src/domain/maze/MazeElement.java
+++ test lauren/src/domain/maze/MazeElement.java
@@ -5,57 +5,57 @@ import domain.Position.Position;
 public abstract class MazeElement {
 	
 	private final int MAZECONSTANT = 40; //zijdelengte van de rastervensters
-	private Position pos1;
-	private Position pos2;
+	private Position position1;
+	private Position position2;
 	
-	public MazeElement(Position pos1, Position pos2, Position pos3){
+	public MazeElement(Position p1, Position p2, Position p3){
 		final double MARGE = 1;
-		if(Math.abs(pos1.getX() - pos2.getX())<=MARGE && Math.abs(pos1.getX() - pos3.getX())<=MARGE){
+		if(Math.abs(p1.getX() - p2.getX())<=MARGE && Math.abs(p1.getX() - p3.getX())<=MARGE){
 			//wall = vertical
-			int x =(int) (Math.floor((pos1.getX())/MAZECONSTANT))*MAZECONSTANT;
-			int lowy = (int) (Math.floor((pos1.getY())/MAZECONSTANT))*MAZECONSTANT;
-			pos1 = new Position(x,lowy);
-			pos2 = new Position(x,lowy+MAZECONSTANT);
+			int x =(int) (Math.floor((p1.getX())/MAZECONSTANT))*MAZECONSTANT;
+			int lowy = (int) (Math.floor((p1.getY())/MAZECONSTANT))*MAZECONSTANT;
+			this.position1 = new Position(x,lowy);
+			this.position2 = new Position(x,lowy+MAZECONSTANT);
 		}
-		else if(Math.abs(pos1.getY() - pos2.getY())<=MARGE && Math.abs(pos1.getY() - pos3.getY())<=MARGE){
+		else if(Math.abs(p1.getY() - p2.getY())<=MARGE && Math.abs(p1.getY() - p3.getY())<=MARGE){
 			//wall = horizontal
-			int y =(int) (Math.floor((pos1.getY())/MAZECONSTANT))*MAZECONSTANT;
-			int lowx = (int) (Math.floor((pos1.getX())/MAZECONSTANT))*MAZECONSTANT;
-			pos1 = new Position(lowx,y);
-			pos2 = new Position(lowx + MAZECONSTANT,y);
+			int y =(int) (Math.floor((p1.getY())/MAZECONSTANT))*MAZECONSTANT;
+			int lowx = (int) (Math.floor((p1.getX())/MAZECONSTANT))*MAZECONSTANT;
+			this.position1 = new Position(lowx,y);
+			this.position2 = new Position(lowx + MAZECONSTANT,y);
 		}
 		else{
 			throw new IllegalArgumentException();
 		}
-		System.out.println("New Wall added from " + pos1 + " to " + pos2);
+		System.out.println("New Wall added from " + this.position1 + " to " + this.position2);
 	}
 	
 	
 
 	public Position getPos1(){
-		return pos1;
+		return position1;
 	}
 	
 	public Position getPos2(){
-		return pos2;
+		return position2;
 	}
 	
 	public boolean hasPosition(Position position){
 		final double MARGE = 0.5;
-		if(pos1.getX() == pos2.getX()){
+		if(position1.getX() == position2.getX()){
 			//wall = vertical
-			if(Math.abs(pos1.getX() - position.getX()) > MARGE) return false;
-			if((pos1.getY() - MARGE<=position.getY() && pos2.getY() + MARGE >= position.getY()) ||
-					(pos1.getY() + MARGE >=position.getY() && pos2.getY() - MARGE <=position.getY())){
+			if(Math.abs(position1.getX() - position.getX()) > MARGE) return false;
+			if((position1.getY() - MARGE<=position.getY() && position2.getY() + MARGE >= position.getY()) ||
+					(position1.getY() + MARGE >=position.getY() && position2.getY() - MARGE <=position.getY())){
 				return true;
 			}
 			else return false;
 		}
-		else if(pos1.getY() == pos2.getY()){
+		else if(position1.getY() == position2.getY()){
 			//wall = horizontal
-			if(Math.abs(pos1.getY() - position.getY()) > MARGE) return false;
-			if((pos1.getX() - MARGE <=position.getX() && pos2.getX() + MARGE >= position.getX()) ||
-					(pos1.getX() + MARGE >=position.getX() && pos2.getX() - MARGE <=position.getX())){
+			if(Math.abs(position1.getY() - position.getY()) > MARGE) return false;
+			if((position1.getX() - MARGE <=position.getX() && position2.getX() + MARGE >= position.getX()) ||
+					(position1.getX() + MARGE >=position.getX() && position2.getX() - MARGE <=position.getX())){
 				return true;
 			}
 			else return false;
