ff6c5ba789f4f1118a9f574773b22198aff39c3e
SergeHendrickx <Serge@10.8.64.96>
2012-11-08 12:07:37 +0100

    moveThread aangepast

--- test lauren/src/domain/robots/MoveThread.java
+++ test lauren/src/domain/robots/MoveThread.java
@@ -6,6 +6,11 @@ public class MoveThread extends Thread {
 	public MoveThread(Movement moveType, SimRobotPilot simRobotPilot){
 		this.moveType= moveType;
 		this.simRobotPilot=simRobotPilot;
+		Thread.UncaughtExceptionHandler h = new Thread.UncaughtExceptionHandler() {
+		    public void uncaughtException(Thread th, Throwable ex) {
+		    }
+		};
+		Thread.setDefaultUncaughtExceptionHandler(h);
 	}
 	
 	@Override
@@ -17,6 +22,8 @@ public class MoveThread extends Thread {
 	public Movement getMovement(){
 		return this.moveType;
 	}
+	
+	
 	
 	
 }
