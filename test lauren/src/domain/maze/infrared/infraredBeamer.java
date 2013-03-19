1d3651c66d874f6e9a2a151719b020c84134ac75
francisduvivier <francisduvivier@gmail.com>
2013-03-19 10:31:59 +0100

    nog wat infrarood

--- test lauren/src/domain/maze/infrared/infraredBeamer.java
+++ test lauren/src/domain/maze/infrared/infraredBeamer.java
@@ -4,9 +4,29 @@ import domain.Position.Position;
 import domain.maze.Orientation;
 
 public class infraredBeamer {
-	private int reach=80;
+	private int reach=80; //standard reach is 80
 	private Orientation orientation;
-	private int frequency;
+	public final int frequency;
 //	private Position pos; //center of infraredBeamer
+	
+	public infraredBeamer(Orientation orientation, int frequency) {
+		this.orientation = orientation;
+		this.frequency = frequency;
+	}
 
+	public int getReach() {
+		return reach;
+	}
+
+	public void setReach(int reach) {
+		this.reach = reach;
+	}
+
+	public Orientation getOrientation() {
+		return orientation;
+	}
+
+	public void setOrientation(Orientation orientation) {
+		this.orientation = orientation;
+	}
 }
