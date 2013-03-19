699bb01835640670a0282bf3045c628afcd41317
francisduvivier <francisduvivier@gmail.com>
2013-03-19 10:22:07 +0100

    infrarood, 1e toevoeging

---
+++ test lauren/src/domain/maze/infrared/infraredBeamer.java
@@ -0,0 +1,12 @@
+package domain.maze.infrared;
+
+import domain.Position.Position;
+import domain.maze.Orientation;
+
+public class infraredBeamer {
+	private int reach=80;
+	private Orientation orientation;
+	private int frequency;
+//	private Position pos; //center of infraredBeamer
+
+}
