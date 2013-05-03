40e0ac844bb9bd6ecf4ea519b3e2d3ccd4748c8d
Lauren De Meyer <lauren.demeyer@student.kuleuven.be>
2013-05-03 08:58:42 +0200

    seesaw fix

--- test lauren/src/gui/ContentPanel.java
+++ test lauren/src/gui/ContentPanel.java
@@ -923,7 +923,7 @@ public class ContentPanel implements ActionListener {
 	    }
 	
 	public void automaticConnection(){
-		controller.readMazeFromFile("test lauren/mazes/UltimateMazeTest.txt");
+		controller.readMazeFromFile("test lauren/mazes/sem2demo2maze");
     	drawingPanel.drawSimulatedWalls();
     	try {
 			Thread.sleep(5000);
